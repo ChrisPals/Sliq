b9b522405db8763a1c24d64f18ca4f26d31e140b
Christoffer Palsgaard <CP@cp-mac.local>
2017-09-08 16:05:08 +0200

    first commit

---
+++ FoodTracker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FoodTracker
+//
+//  Created by Jane Appleseed on 10/17/16.
+//  Copyright © 2016 Apple Inc. All rights reserved.
+//
+
+import UIKit
+import FacebookLogin
+import FacebookCore
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
